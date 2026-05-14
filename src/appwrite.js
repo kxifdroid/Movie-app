98660e0fbe76106abe85e1aa65657fae1333e90e
kxifdroid <kxifiit@gmail.com>
2026-05-14 17:14:35 +0530

    initial commit

---
+++ src/appwrite.js
@@ -0,0 +1,44 @@
+import { Client, Databases, Query, ID } from 'appwrite'
+import searchTerm from './App.jsx'
+import movie from './components/MovieCard.jsx'
+
+
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID
+
+const client = new Client()
+.setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+.setProject(PROJECT_ID)
+
+const databases = new Databases(client)
+
+export const updateSearchCount = async(searchTerm, movie) => {
+  try {
+    const result = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.equal('searchTerm', searchTerm)])
+
+    if(result.documents.length > 0) {
+      const doc = result.documents[0]
+      await databases.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {count: doc.count + 1})
+    }else{
+      await databases.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movie_id: movie.id,
+        poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+      })
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getTrendingMovies = async() => {
+  try {
+    const result = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.orderDesc('count'), Query.limit(5)])
+    return result.documents
+  } catch (error) {
+    console.log(error)
+  }
+}
